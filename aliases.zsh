b95c9cd2af4b16270157644917b9dd0b78e02904
Ying Tu <yingtu35@gmail.com>
2026-02-03 23:13:08 -0800

    Update .zshrc and aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,30 +3,19 @@ alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="omz reload"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
-alias compile="commit 'compile'"
+# alias compile="commit 'compile'"
 alias timestamp="date +%s"
-alias version="commit 'version'"
+# alias version="commit 'version'"
+alias car=cat
+alias cl=clear
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias projects="cd $HOME/Code"
-alias sites="cd $HOME/Herd"
+# alias projects="cd $HOME/Code"
+# alias sites="cd $HOME/Herd"
 
-# Laravel
-alias a="herd php artisan"
-alias fresh="herd php artisan migrate:fresh --seed"
-alias tinker="herd php artisan tinker"
-alias seed="herd php artisan db:seed"
-alias serve="herd php artisan serve"
-
-# PHP
-alias cfresh="rm -rf vendor/ composer.lock && composer i"
-alias composer="herd composer"
-alias php="herd php"
-alias test="pest --no-coverage"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
@@ -35,8 +24,6 @@ alias watch="npm run dev"
 # Docker
 alias docker-composer="docker-compose"
 
-# SQL Server
-alias mssql="docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=LaravelWow1986! -p 1433:1433 mcr.microsoft.com/mssql/server:2017-latest"
 
 # Git
 alias gs="git status"
@@ -55,4 +42,4 @@ alias push="git push"
 alias resolve="git add . && git commit --no-edit"
 alias stash="git stash -u"
 alias unstage="git restore --staged ."
-alias wip="commit wip"
+# alias wip="commit wip"
